97b04a6ac5281666f17a392889a6a59b6c3692f9
EliasColeWest <90935144+EliasColeWest@users.noreply.github.com>
2021-11-15 02:35:44 -0600

    Create EjercicioB.c
    Ejercicio B

---
+++ EjercicioB.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //srand(time(NULL));
+    float COS[12], promedio, sum, mayor, menor;
+    int contador, mayorcosecha, mayormes, menormes;
+    mayor = menor = COS[0];
+
+    while (contador<12)
+    {
+    	COS[contador]=rand() %100;
+    	
+        printf("COS %f \n", COS[contador]);//Quitar luego
+        sum = sum + COS[contador];
+        promedio = sum/12;
+        printf("SUMA = %f \n", sum);//Quitar luego
+        
+        contador++;
+    }
+    
+    printf("SUMA = %f \n", promedio);
+    contador = 0;
+    while(contador<12){
+    	if(COS[contador]>promedio){
+        	mayorcosecha=mayorcosecha+1;
+        	
+		}
+		
+		if (COS[contador] > mayor){
+			mayor = COS[contador];
+			mayormes=contador;
+		} 
+		
+		if (COS[contador] < menor){
+		 	menor = COS[contador];
+		 	menormes=contador;
+		}
+		
+		contador++;
+	}
+	printf("Mayor que el promedio anual = %d \n", mayorcosecha);
+	printf("Mes con mayor cosecha fue el %d con %f \n", mayormes, mayor);
+	printf("Mes con menor cosecha fue el %d con %f \n", menormes, menor);
+
+}
